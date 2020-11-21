22384ca039b981f69ee2b6ffe7f5213fa7e73968
shanu17 <suk117@pitt.edu>
2020-11-21 12:42:11 -0500

    Using axios for await/async

--- routes/pages.js
+++ routes/pages.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const request = require("request");
 const fs = require('fs');
+const fsPromises = fs.promises;
 var cors = require('cors');
+const axios = require('axios')
 
 const router = express.Router();
 
@@ -9,77 +11,72 @@ router.get("/", cors(),(req, res) => {
 	res.render("index");
 });
 
-router.post("/search", cors(), (req, res) => {
+router.post("/search", cors(), async (req, res) => {
 	const {token} = req.body;
 	var userDir;
-	if (token) {
-		let url = "https://canvas.pitt.edu/api/v1/users/self/profile?access_token=" + token;
-		request.get(url, { json: true }, (error, response, body) => {
-			if(error)
-				return res.status(404).send("Error accessing Canvas user");
-			if(response.statusCode == 200) {
-				let userProfile = body;
-				userDir = "./" + userProfile.id;
-				if(!fs.existsSync(userDir))
-					fs.mkdirSync(userDir);
-				let courseUrl = "https://canvas.pitt.edu/api/v1/users/self/courses?access_token=" + token;
-				request.get(courseUrl, {json: true}, (error, response, body) => {
-					if(error)
-						return res.status(404).send("Error accessing Courses for Canvas User ID" + userProfile.id);
-					if(response.statusCode == 200) {
-						let courseList = body;
-						for(let i in courseList) {
-							let moduleUrl = "https://canvas.pitt.edu/api/v1/courses/" + courseList[i].id + "/modules?access_token=" + token + "&per_page=20	";
-							request.get(moduleUrl, {json: true}, (error, response, body) => {
-								if(error)
-									return res.status(404).send("Error accessing modules of Course " + courseList[i].name);
-								if(response.statusCode = 200) {
-									let moduleList = body;
-									if(moduleList.length > 0) {
-										for(let j in moduleList) {
-											let itemsUrl = moduleList[j].items_url + "?access_token=" + token;
-											request.get(itemsUrl, {json: true}, async (error, response, body) => {
-												if(error)
-													return res.status(404).send("Error accessing Module Items");
-												let itemsList = body;
-												for(let k in itemsList) {
-													if(itemsList[k].title.includes(".pdf") || itemsList[k].title.includes(".ppt")) {
-														let fileUrl = itemsList[k].url + "?access_token=" + token;
-														await new Promise(r => setTimeout(r, 200));
-														request.get(fileUrl, {json: true}, (error, response, body) => {
-															if(error)
-																return res.status(404).send("Error accessing files in Module Items");
-															let files = body;
-															let downloadPath = userDir + "/" + files.filename;
-															if(!fs.existsSync(downloadPath)) {
-																console.log("inside if");
-																request.get(files.url).on("error", (err) => {
-																	console.log(err);
-																}).pipe(fs.createWriteStream(downloadPath));
-															}
-														});
-													}
-												}
-											});
-										}
-									}
-								} else
-								return res.status(404).send("Error cannot retrieve Modules for Course " + courseList[i].name);
-							});
-						}
-						res.render("search", {user: userProfile.id});
-					} else {
-						return res.status(404).send("Error cannot retrieve Courses for User ID" + userProfile.id);
-					}
-				});
-			} else {
-				return res.status(404).send("Error Invalid Canvas user");
+	if (!token) {
+		return res.redirect("/");
+	}
+
+	let url = "https://canvas.pitt.edu/api/v1/users/self/profile?access_token=" + token;
+	try {
+		let response = await axios.get(url)
+		if (response.status !== 200) {
+			return res.status(404).send("Error Invalid Canvas user");
+		}
+
+		let body = response.data
+		let userProfile = body;
+		userDir = "./" + userProfile.id;
+		if(!fs.existsSync(userDir))
+			fs.mkdirSync(userDir);
+		let courseUrl = "https://canvas.pitt.edu/api/v1/users/self/courses?access_token=" + token;
+		response = await axios.get(courseUrl);
+		if(response.status !== 200) {
+			return res.status(404).send("Error cannot retrieve Courses for User ID" + userProfile.id);
+		}
+		body = response.data;
+		let courseList = body;
+		for(let i in courseList) {
+			let moduleUrl = "https://canvas.pitt.edu/api/v1/courses/" + courseList[i].id + "/modules?access_token=" + token + "&per_page=20	";
+			response = await axios.get(moduleUrl);
+			if(response.status !== 200) {
+				return res.status(404).send("Error cannot retrieve Modules for Course " + courseList[i].name);
 			}
-			console.log(userDir); // U can access the userDir created here 
-			// Do all the python script stuff for indexing here by using this
-		});
-	} else {
-		res.redirect("/");
+			body = response.data;
+			let moduleList = body;
+			if(moduleList.length > 0) {
+				for(let j in moduleList) {
+					let itemsUrl = moduleList[j].items_url + "?access_token=" + token;
+					let response = await axios.get(itemsUrl);
+					if(response.status !== 200) {
+						return res.status(404).send("Error cannot retrieve Modules items for Course ");
+					}
+					body = response.data;
+					let itemsList = body;
+					for(let k in itemsList) {
+						if(itemsList[k].title.includes(".pdf") || itemsList[k].title.includes(".ppt")) {
+							let fileUrl = itemsList[k].url + "?access_token=" + token;
+							response = await axios.get(fileUrl);
+							if(response.status !== 200) {
+								return res.status(404).send("Error accessing files in Module Items");
+							}
+							body = response.data;
+							let files = body;
+							let downloadPath = userDir + "/" + files.filename;
+							if(!fs.existsSync(downloadPath)) {
+								let fileContents = axios.get(files.url);
+								await fsPromises.writeFile(downloadPath, fileContents);
+							}
+						}
+					}
+				}
+			}
+		}
+		res.render("search", {user: userProfile.id});
+	}
+	catch (error) {
+		return res.status(404).send("Error accessing Canvas user");
 	}
 });
 
